6599970ed8e96f531c97a5fe1d039cf28b5876bf
okyang <owenyang123@gmail.com>
2019-09-24 13:46:51 -0700

    added weight measurement

--- ZBinClassDev.py
+++ ZBinClassDev.py
@@ -32,42 +32,62 @@ HX711OUT = 6	  #weight sensor out
 
 
 class ZotBins():
-    def __init__(self,sendData=True,frequencySec=600,sim=False):
+    def __init__(self,sendData=True,frequencySec=600):
         """
         sendData<Bool>: determines whether or not the algortihm should
             send data to the tippers database or
             just save the data locally.
         frequencySec<int>: determines the sampling rate of which the
             algorithm collects data
-        sim<bool>: if there are no sensors, users may simulate getting data
-            from the sensors by assigning this variable as True.
         """
-        #========class variables for data collection algorithm=========
-        self.sendData=sendData
-        self.frequencySec=frequencySec
+        #extract the json info
+        bininfo = self.parseJSON()
 
-        #====set up GPIO pins============
-        GPIO.setmode(GPIO.BCM) #for weight sensor
+        #====set up Ultrasonic GPIO pins============
         GPIO.setup(GPIO_TRIGGER, GPIO.OUT) #for ultrasonic sensor
         GPIO.setup(GPIO_ECHO, GPIO.IN) #for ultrasonic sensor
 
+        #=====setup hx711 GPIO pins=================
+        GPIO.setmode(GPIO.BCM) #for weight sensor
+        self.hx = HX711(HX711IN, HX711OUT)
+        self.hx.set_reading_format("LSB", "MSB")
+        self.hx.set_reference_unit(float( bininfo["weightCal"] ))
+        self.hx.reset()
+        self.hx.tare()
 
-    def run(self):
+        #========class variables for data collection algorithm=========
+        #generic
+        self.sendData=sendData
+        self.frequencySec=frequencySec
+
+        #========Query Information======================================
+        #assign variables
+        self.binID = bininfo["binID"]
+        self.weightSensorID = self.binID
+        self.weightType = 2
+        self.ultrasonicSensorID = self.binID + 'D'
+        self.ultrasonicType = 3
+        self.headers = {
+        	"Content-Type": "application/json",
+        	"Accept": "application/json"
+        }
+
+    def run(self,sim=False):
         """
         Runs the data collection algorithm
+        sim<bool>: if there are no sensors, users may simulate getting data
+            from the sensors by assigning this variable as True.
         """
-        #=======Parse JSON=========
-        bininfo = self.parseJSON()
-        print(bininfo)
-
         #=======MAIN LOOP==========
         while True:
 
             #========Measure the Distance==============================
-            x = self.measure_dist()
+            x = self.measure_dist(sim)
             print(x)
 
             #=========Measure the Weight===============================
+            y = self.measure_weight(sim)
+            print(y)
 
             #=========Format the data==================================
 
@@ -80,6 +100,27 @@ class ZotBins():
 
             #temporary for testing
             break
+
+    def measure_weight(self,simulate=False):
+        """
+        This function measures the weight. It measures the weights 11 times,
+        sorts it, and returns the median.
+        """
+        if siumulate:
+            return 0.0
+        else:
+            #array to collect the weight measurements
+            derek = []
+
+            #collect a list of weight measurements
+            for i in range(11):
+                derek.append(self.hx.get_weight(5))
+                self.hx.power_down()
+                self.hx.power_up()
+                time.sleep(0.5)
+
+            return sorted(derek)[5]
+
 
     def measure_dist(self,simulate=False):
         """
@@ -133,4 +174,8 @@ class ZotBins():
 
 if __name__ == "__main__":
     zot = ZotBins(sendData=True,frequencySec=10,sim=False) #initialize the ZotBins object
-    zot.run() #run the data collection algorithm
+    try:
+        zot.run() #run the data collection algorithm
+    finally:
+        GPIO.cleanup()
+        sys.exit()
