e74c969dbc4897dc0243dca50b5113fa35b2c336
okyang <okyang@uci.edu>
2020-04-05 11:56:14 -0700

    updates to testing

--- Sensor_Testing/Arduino_Weight/serialWeightReading.py
+++ Sensor_Testing/Arduino_Weight/serialWeightReading.py
@@ -1,10 +1,11 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyACMO=0',9600)
+ser = serial.Serial('/dev/ttyACM0',9600)
 
-try:
-    read_serial=ser.readline()
-    print(str(read_serial,'utf-8').rstrip())
-except serial.serialutil.SerialException:
-    time.sleep(2)
+while True:
+    try:
+        read_serial=ser.readline()
+        print(str(read_serial, 'utf-8').rstrip())
+    except serial.serialutil.SerialException:
+        time.sleep(2)
