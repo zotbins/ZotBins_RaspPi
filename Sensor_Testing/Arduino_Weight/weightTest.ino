21d2b20cf16fadc0052597500ec91d07407cc49b
Owen Yang <33404602+okyang@users.noreply.github.com>
2020-04-16 21:29:34 -0700

    no extra print statements

--- Sensor_Testing/Arduino_Weight/weightTest.ino
+++ Sensor_Testing/Arduino_Weight/weightTest.ino
@@ -33,18 +33,14 @@ HX711 scale;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 scale demo");
 
   scale.begin(DOUT, CLK);
   scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
 
-  Serial.println("Readings:");
 }
 
 void loop() {
-  Serial.print("Reading: ");
   Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
-  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
   Serial.println();
 }
