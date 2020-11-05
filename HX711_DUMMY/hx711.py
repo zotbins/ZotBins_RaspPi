0434037eb047cc0a2bb4be515f45e3406a2c6fff
gracechoe <gracechoe925@gmail.com>
2020-11-05 10:58:18 -0800

    Add simulation stuff

---
+++ HX711_DUMMY/hx711.py
@@ -0,0 +1,2 @@
+def HX711(input, output):
+   return
---
+++ RPi_DUMMY/GPIO.py
@@ -0,0 +1,19 @@
+BOARD = 1
+OUT = 1
+IN = 1
+BCM = 1
+
+def setmode(a):
+   pass
+def setup(a, b):
+   pass
+def output(a, b):
+   pass
+def input(a):
+   pass
+def cleanup():
+   pass
+def setmode(a):
+   pass
+def setwarnings(flag):
+   pass
